c1e5be4310dcb8cf1fd50f86ba1f3d84721c3abb
udithindrakantha <udith.indrakantha@gmail.com>
2019-09-07 10:00:25 +0530

    Asset Registration moved to another service class

--- Backend 1.0/src/main/java/com/project/inventoryManagement/Controllers/AssetController.java
+++ Backend 1.0/src/main/java/com/project/inventoryManagement/Controllers/AssetController.java
@@ -4,6 +4,7 @@ package com.project.inventoryManagement.Controllers;
 import com.project.inventoryManagement.Models.AssetModel;
 import com.project.inventoryManagement.Repositories.AssetRepository;
 import com.project.inventoryManagement.Repositories.IDNumberRepo;
+import com.project.inventoryManagement.Services.AssetRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,9 @@ public class    AssetController {
 
     @Autowired
     private AssetRepository assetRepo;
+
+    @Autowired
+    AssetRegistration assetRegistration;
 
     @Autowired
     private IDNumberRepo idNumberRepo;
@@ -39,42 +43,10 @@ public class    AssetController {
    // Register a new asset
     @PostMapping(path="/add") // Map ONLY POST Requests
     public AssetModel addNewAsset(@RequestBody AssetModel aa1) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        //creating an aasset id according to the category
-
        System.out.println(aa1.toString());
 
-        System.out.println("line0");
+       return assetRegistration.registerAnAsset(aa1);
 
-        if(aa1.getAssetCategory().equals("Laptop")){
-            aa1.setAssetId("LAP");
-        }else if(aa1.getAssetCategory().equals("PC")){
-            aa1.setAssetId("PC");
-        }else if(aa1.getAssetCategory().equals("Projector")){
-            aa1.setAssetId("PRJ");
-        }else if(aa1.getAssetCategory().startsWith("Fur") || aa1.getAssetCategory().startsWith("fur")){
-            aa1.setAssetId("FUR");
-        }else {
-            aa1.setAssetId("OTH");
-        }
-
-        System.out.println("line00");
-        aa1.toString();
-
-        long oldId = idNumberRepo.findFirst().getAssetIdNumber();
-        System.out.println("line111");
-        aa1.setAssetId(aa1.getAssetId()+oldId);
-        System.out.println("line222");
-       int x = idNumberRepo.updateAssetId(oldId,++oldId);
-        System.out.println("line333");
-
-        aa1.setBroken(false);
-
-        assetRepo.save(aa1);
-        System.out.println("line1444");
-        return aa1;
     }
  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
---
+++ Backend 1.0/src/main/java/com/project/inventoryManagement/Services/AssetRegistration.java
@@ -0,0 +1,60 @@
+package com.project.inventoryManagement.Services;
+
+import com.project.inventoryManagement.Models.AssetModel;
+import com.project.inventoryManagement.Repositories.AssetRepository;
+import com.project.inventoryManagement.Repositories.IDNumberRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssetRegistration {
+
+    @Autowired
+    AssetRepository assetRepository;
+
+    @Autowired
+    private IDNumberRepo idNumberRepo;
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Asset registration
+    public AssetModel registerAnAsset(AssetModel aa1) {
+
+        //creating an aasset id according to the category
+
+        System.out.println(aa1.toString());
+
+        System.out.println("line0");
+
+        if(aa1.getAssetCategory().equals("Laptop")){
+            aa1.setAssetId("LAP");
+        }else if(aa1.getAssetCategory().equals("PC")){
+            aa1.setAssetId("PC");
+        }else if(aa1.getAssetCategory().equals("Projector")){
+            aa1.setAssetId("PRJ");
+        }else if(aa1.getAssetCategory().startsWith("Fur") || aa1.getAssetCategory().startsWith("fur")){
+            aa1.setAssetId("FUR");
+        }else {
+            aa1.setAssetId("OTH");
+        }
+
+        System.out.println("line00");
+        aa1.toString();
+
+        long oldId = idNumberRepo.findFirst().getAssetIdNumber();
+        System.out.println("line111");
+        aa1.setAssetId(aa1.getAssetId()+oldId);
+        System.out.println("line222");
+        int x = idNumberRepo.updateAssetId(oldId,++oldId);
+        System.out.println("line333");
+
+        aa1.setBroken(false);
+
+        assetRepository.save(aa1);
+        System.out.println("line1444");
+        return aa1;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
