d3b125bed9a1b87ceac33546b3a5504dc53ef399
udithindrakantha <udith.indrakantha@gmail.com>
2019-09-25 16:52:01 +0530

    no message

--- Frontend 1.0/src/app/AssetCatagary/other/other.component.ts
+++ Frontend 1.0/src/app/AssetCatagary/other/other.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Asset } from '../../asset/asset';
 import { HttpService } from '../../service2/http.service';
-import { addAllToArray } from '@angular/core/src/render3/util';
+
 
 @Component({
   selector: 'app-other',
@@ -12,15 +12,15 @@ export class OtherComponent implements OnInit {
   model:any ={}
   asset: Asset;
 
-  constructor(private userService:HttpService) { 
-      this.asset=new Asset(); 
+  constructor(private userService:HttpService) {
+      this.asset=new Asset();
       this.asset.assetcategory="Other";
       console.log(this.asset);
 
   }
 
   ngOnInit() {
-    
+
   }
   onSubmit() {
     console.log(this.asset);
