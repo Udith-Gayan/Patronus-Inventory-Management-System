598344bb6a1a6ead6911ccf292b330d2c7702468
madhusanka <sajithedirimanna75@gmail.com>
2019-10-02 21:13:32 +0530

    P 915pm 10/2/2019  get mabushnaka 8 branch

--- Frontend 1.0/src/app/upgrade/view-all-asset/view-all-asset.component.ts
+++ Frontend 1.0/src/app/upgrade/view-all-asset/view-all-asset.component.ts
@@ -10,23 +10,31 @@ import { Session } from 'inspector';
   templateUrl: './view-all-asset.component.html',
   styleUrls: ['./view-all-asset.component.scss']
 })
+
+
+
 export class ViewAllAssetComponent implements OnInit {
   email:string = sessionStorage.getItem('email');
   Fname:string = sessionStorage.getItem('firstname');
   Lname:string = sessionStorage.getItem('lastname');
   status:string = sessionStorage.getItem('status');
-
+today=new Date();
   pendingRequestAM:Observable<pendinRequest>
   constructor(private asset:HttpService) { }
 AllAsset:Observable<Asset>
 
   ngOnInit() {
+   
     this.asset.getAllAssets().subscribe(res=>{
 
       console.log(res);
       this.AllAsset = res
       console.log(this.AllAsset)
     })
+  }
+
+  onclick(){
+    window.print();
   }
 
 }
